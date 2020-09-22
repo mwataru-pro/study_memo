b001ba2abe69890ed3f83f7925239aa11974f293
mwataru-pro <m1130wata@gmail.com>
2020-09-22 11:17:43 +0900

    最小値の算出

---
+++ paiza/kakezan.rb
@@ -0,0 +1,21 @@
+# 【paiza練習問題】
+
+# 掛け算
+a = gets.to_i
+b = gets.to_i
+puts a * b
+
+# 足し算
+a,b = gets.chomp.split(" ")
+puts a.to_i + b.to_i
+
+# 最小値を求める
+n_1 = gets.to_i
+n_2 = gets.to_i
+n_3 = gets.to_i
+n_4 = gets.to_i
+n_5 = gets.to_i
+
+n = [n_1,n_2,n_3,n_4,n_5]
+
+puts n.min
